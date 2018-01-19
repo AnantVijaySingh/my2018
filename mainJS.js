adf9a87173983fdc1b3724cfb2bb2f0c5c088257
Anant <anantvijay.23@gmail.com>
2018-01-19 22:17:20 +0000

    Defining Goal Objectes

--- mainJS.js
+++ mainJS.js
@@ -9,12 +9,31 @@ var goals = ["No binge watching",
 	"Travel to one new country",
 	"Maintain a prioritized to-do list"]
 
-window.onload = createGoalsList;
+var goalsObj = []
+
+window.onload = init;
+
+function init() {
+	createGoalsObjects();
+	createGoalsList();
+};
 
 function createGoalsList() {
 	for (var i = 0; i < goals.length; i++) {
 		var mainDiv = document.getElementById("listDivId");
 		mainDiv.innerHTML = goals[i];
 	};
-}
+};
 
+function createGoalsObjects() {
+	for (var i = 0; i < goals.length; i++) {
+		goalsObj[i] = {
+			id: i+1,
+			goal: goals[i],
+			goalType: 
+			goalRemaining: 
+			status:
+		};
+		console.log(goalsObj[i]);
+	};
+};
