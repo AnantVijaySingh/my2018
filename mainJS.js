6843cfb006eb14aaaf7ff1f1442c09098a207ba6
Anant <anantvijay.23@gmail.com>
2018-01-25 23:55:06 +0000

    definning structre

--- mainJS.js
+++ mainJS.js
@@ -1,3 +1,11 @@
+/*
+STRUTURE
+-- Pre define goals statement array
+-- define the an array that holds the goal objects for each goal
+-- iterate over this array of goals objects and pass the properties of each goal to a goals div box creator
+-- the goals div box creator function pushes the content of each goal into a div and injects it in the HTML 
+*/
+
 // Global Variables
 
 var goals = ["No binge watching", 
@@ -55,8 +63,10 @@ function createGoalsObjects() {
 			type: getGoalType(i),
 			status: getGoalStatus(i)
 		};
-		console.log(goalsObj[i]);
+		// console.log(goalsObj[1]);
 	};
+	console.log(goalsObj[2].goal);
+	createGoalsDivBoxes();
 };
 
 
@@ -72,6 +82,18 @@ function getGoalStatus (id) {
 	return "status"
 };
 
+
+
+
+// Iterate over the object and add the values to the goals div boxes
+function createGoalsDivBoxes() {
+	goalsObj.forEach( function(element, index) {
+		function injectGoalDivBoxes
+		for (property in element) {
+			console.log(element[property]);
+		}
+	});
+}
 
 
 // DOM Functions
