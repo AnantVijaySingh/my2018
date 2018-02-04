385aa27234f7fe607b423e5c10bce6dca748181e
Anant <anantvijay.23@gmail.com>
2018-02-04 23:07:45 +0000

    location https issue fix

--- location.js
+++ location.js
@@ -47,7 +47,7 @@ function getAddress(latitude,longitude){
 	var request = new XMLHttpRequest();
 
 	var method = 'GET';
-	var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true';
+	var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true';
 	var async = true;
 
 	request.open(method, url, async);
