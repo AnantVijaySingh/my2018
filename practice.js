d2b8160f492ee8e1256c7c0785e66471d0aebe9d
Anant <anantvijay.23@gmail.com>
2018-02-11 15:13:49 +0000

    canvas 2

--- practice.js
+++ practice.js
@@ -1,3 +1,7 @@
+// --------------------------------
+// API Practice
+// --------------------------------
+
 // var url = "https://github.com/AnantVijaySingh/my2018/blob/master/API/practiceData.json"
 var url = "API/practiceData.json"
 var request = new XMLHttpRequest();
@@ -14,3 +18,46 @@ function getData () {
 	request.open("GET",url);
 	request.send(null); 
 }
+
+
+
+
+
+// ---------------------------------------
+// Canvas Practice
+// ---------------------------------------
+
+// function canvasFunction () {
+// 	var canvas = document.getElementById("canvasElement");
+// 	var context = canvas.getContext("2d");
+// 	var previewBtn = document.getElementById("previewBtn");
+// 	console.log(previewBtn);
+// 	previewBtn.addEventListener("click", previewBtnHandler,false)
+// 	previewBtn.canvas = canvas;
+// 	previewBtn.context = context;
+
+
+// 	// context.fillStyle = "blue";
+// 	// context.fillRect(10,10,100,100);
+// }
+
+// function previewBtnHandler (event) {
+// 	// console.log(event.currentTarget.canvas);
+// 	var selectObj = document.getElementById("shape");
+// 	var index = selectObj.selectedIndex;
+// 	var shape = selectObj[index].value;
+
+// 	if (shape == "square" ) {
+// 		for(var i = 0; i < 20; i++){
+// 			drawSquare(event.currentTarget.canvas, event.currentTarget.context);	
+// 		}
+// 	} else {
+// 		// statement
+// 	}
+// }
+
+// function drawSquare(canvas,context) {
+// 	var w = Math.floor(Math.random()*40);
+// 	var x = Math.floor(Math.random()*canvas.width);
+// 	var y = Math.floor(Math.random()*canvas.height);
+// }
--- practice_1.js
+++ practice_1.js
@@ -1,4 +1,5 @@
-function canvasFunction (argument) {
+
+function canvasFunction () {
 	var canvas = document.getElementById("canvasElement");
 	var context = canvas.getContext("2d");
 	var previewBtn = document.getElementById("previewBtn");
@@ -13,14 +14,30 @@ function canvasFunction (argument) {
 }
 
 function previewBtnHandler (event) {
-	console.log(event.currentTarget.canvas);
+	// console.log(event.currentTarget.canvas);
+	var canvas = event.currentTarget.canvas;
+	var context = event.currentTarget.context;
 	var selectObj = document.getElementById("shape");
 	var index = selectObj.selectedIndex;
 	var shape = selectObj[index].value;
-
+	console.log("button press captured");
+	context.fillStyle = "white";
+	context.fillRect(0,0,canvas.width,canvas.height);
 	if (shape == "square" ) {
-		
+		for(var i = 0; i < 20; i++){
+			drawSquare(event.currentTarget.canvas, event.currentTarget.context);	
+		}
 	} else {
 		// statement
 	}
+}
+
+function drawSquare(canvas,context) {
+	console.log("drawing square");
+	var w = Math.floor(Math.random()*40);
+	var x = Math.floor(Math.random()*canvas.width);
+	var y = Math.floor(Math.random()*canvas.height);
+
+	context.fillStyle = "blue";
+	context.fillRect(x,y,w,w);
 }
